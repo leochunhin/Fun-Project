30a34efe6540e6506f4ae4f687b93cec18fb7b9e
leochunhin <96699677+leochunhin@users.noreply.github.com>
2021-12-27 03:27:19 +0800

    Add files via upload

--- amaziograph.py
+++ amaziograph.py
@@ -1,22 +1,22 @@
-# Done by Lein, Chun Hin SID:20693425
+
 import turtle
 
-turtle.setup(800,600)    # Set the width and height be 800 x 600
+turtle.setup(800,600)    
 
-number_of_divisions = 8  # The number of subdivisions around the centre
-turtle_width = 3         # The width of the turtles
+number_of_divisions = 8  
+turtle_width = 3         
 
-# Don't show the animation
+
 turtle.tracer(False)
 
-# Draw the background lines
+
 
 backgroundTurtle = turtle.Turtle()
 
 backgroundTurtle.width(1)
 
 backgroundTurtle.down()
-backgroundTurtle.color("gray88") # Draw the centered line
+backgroundTurtle.color("gray88") 
 for i in range(number_of_divisions):
     backgroundTurtle.forward(500)
     backgroundTurtle.backward(500)
@@ -24,7 +24,7 @@ for i in range(number_of_divisions):
 
 backgroundTurtle.up()
 
-# Show the instructions
+
 backgroundTurtle.color("purple")
 backgroundTurtle.goto(-turtle.window_width()/2+50, 100)
 backgroundTurtle.write("""s - change a colour for one of the colour buttons
@@ -34,23 +34,23 @@ c - clear all drawings made by the 8 drawing turtles
 
 backgroundTurtle.hideturtle()
 
-# Set up a turtle for handling message on the turtle screen
+
 messageTurtle = turtle.Turtle()
-# This sets the colour of the text to red
+
 messageTurtle.color("red")
-# We do not want it to show/draw anything, except the message text
+
 messageTurtle.up() 
-# Set it the be at center, near the colour selections
+
 messageTurtle.goto(0, -200)
-# We do not want to show it on the screen
+
 messageTurtle.hideturtle()
 
-# Part 2 Preparing the drawing turtles
 
-# The drawing turtles are put in a list
+
+
 allDrawingTurtles = [] 
 
-# Part 2.1 Add the 8 turtles in the list
+
 for _ in range(number_of_divisions):
     newTurtle = turtle.Turtle()
     newTurtle.speed(0)
@@ -59,14 +59,13 @@ for _ in range(number_of_divisions):
 
     allDrawingTurtles.append(newTurtle)
     
-# Part 2.2 Set up the first turtle for drawing
+
 dragTurtle = allDrawingTurtles[0]
 dragTurtle.showturtle()
 dragTurtle.shape('circle')
 dragTurtle.shapesize(2,2)
 
-# Part 3 Preparing the basic drawing system
-# Set up the ondrag event
+
 def draw(x,y):
     dragTurtle.ondrag(None)
     turtle.tracer(False)
@@ -91,7 +90,7 @@ def draw(x,y):
 dragTurtle.ondrag(draw)
 
 
-# Part 5.2 clear all drawings made by the 8 drawing turtles
+
 def clearDrawing():
     for drawingTurtle in allDrawingTurtles:
         drawingTurtle.clear()
@@ -102,7 +101,6 @@ def clearDrawing():
 turtle.onkeypress(clearDrawing, 'c')
 
 
-# Part 5.3 all 8 drawing turtles go to middle
 def goToMiddle():
     for drawingTurtle in allDrawingTurtles:
         drawingTurtle.up()
@@ -114,13 +112,11 @@ def goToMiddle():
     
 turtle.onkeypress(goToMiddle, 'm')
 
-# Part 4 handling the colour selection
-# Make the colour selection turtles
-# Here is the list of colours
+
 colours = ["black", "orange red", "lawn green", "medium purple",\
            "light sky blue", "orchid", "gold"]
 
-# Part 4.2 Set up the onclick event
+
 def handleColourChange(x,y):
     if x<=-130:
         for thisturtle in allDrawingTurtles:
@@ -145,7 +141,7 @@ def handleColourChange(x,y):
             thisturtle.color(colours[6])
         
     
-# Part 5.4 change a colour in the colour selection
+
 def changeColour():
     a = turtle.textinput('Change color',\
                      'Please type the index number for the turtle:(0-6)')
@@ -167,7 +163,7 @@ def changeColour():
 
 turtle.onkeypress(changeColour, 's')
 
-# Part 4.1 Make the colour selection turtles
+
 colourSelectionTurtles = []
 
 for i in range(len(colours)):
